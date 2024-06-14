bb7b6ae5901e4081ab0c769aebf346138be2f653
Abdieljrg <abdieljrg@gmail.com>
2024-06-14 15:17:53 -0400

    entering review update and comments for better understanding <3

--- model/review.py
+++ model/review.py
@@ -1,12 +1,31 @@
+""" reviews"""
+
 import uuid
 from datetime import datetime
 
 class Review:
-    def __init__(self, user, place, text, rating):
+    """clase para los reviews / class for review"""
+    def __init__(self, user, place, text, rating, comment):
+        """ genera el id / creates ID"""
         self.id = str(uuid.uuid4())
         self.user = user
         self.place = place
         self.text = text
         self.rating = rating
+        self.comment = comment
+        """ tiempo de creacion y update / time of creation and update"""
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """retorna data como diccionario / returns the data as dictionary"""
+        return {
+            'review_id': self.review_id,
+            'user_id': self.user_id,
+            'place_id': self.place_id,
+            'rating': self.rating,
+            'comment': self.comment,
+            """ convierte el date en ISOformat / converts the date time as ISO format"""
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat()
+        }

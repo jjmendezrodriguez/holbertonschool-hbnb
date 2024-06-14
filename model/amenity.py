f4d726c7408ab666702cc83df0623c7a08a43a70
Abdieljrg <abdieljrg@gmail.com>
2024-06-14 14:39:47 -0400

    updating amenitys for better understanding and routing it

--- model/amenity.py
+++ model/amenity.py
@@ -1,10 +1,24 @@
+""" amenidades / amenitys"""
+
+
 import uuid
 from datetime import datetime
 
 class Amenity:
     def __init__(self, name, description):
+        """ identificacion"""
         self.id = str(uuid.uuid4())
         self.name = name
         self.description = description
+        """tiempo de creacion"""
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+    def to_dict(self):
+        return {
+            'amenity_id': self.amenity_id,
+            'name': self.name,
+            """ convertir el tiempo en ISOformat"""
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat()
+        }

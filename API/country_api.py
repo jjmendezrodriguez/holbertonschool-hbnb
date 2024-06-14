79ced7b6710f3adb3d64fcedf11ddeba7f81c2b0
jjmendezrodriguez <jjmendezrodriguez@yahoo.com>
2024-06-14 18:43:47 -0400

    fix code country_api add comments and modify class for better functions

--- API/country_api.py
+++ API/country_api.py
@@ -1,33 +1,71 @@
+from flask import request
 from flask_restx import Namespace, Resource, fields
-from model.country import Country
 from data_manager import DataManager
+import uuid
+from datetime import datetime
 
+# Definición del namespace para las country.
 ns = Namespace('countries', description='Operations related to countries')
-
-country_model = ns.model('Country', {
-    'name': fields.String(required=True, description='The country name'),
-    'code': fields.String(required=True, description='The country code')
-})
-
 data_manager = DataManager()
 
+# Definición del modelo para una Country
+country_model = ns.model('Country', {
+    'id': fields.String(required=True, description='The country ID'),
+    'name': fields.String(required=True, description='The country name'),
+    'created_at': fields.DateTime(required=True, description='Date and time when country was created'),
+    'updated_at': fields.DateTime(required=True, description='Date and time country was last updated')
+})
+
+# Ruta para gestionar la colección de countries
 @ns.route('/')
-class CountryList(Resource):
-    @ns.doc('list_countries')
+class Countries(Resource):
     @ns.marshal_list_with(country_model)
     def get(self):
-        """List all countries"""
-        countries = data_manager.get_all('country')
-        return countries, 200
+        return data_manager.get_all_countries()
 
-@ns.route('/<string:country_code>')
-@ns.param('country_code', 'The country identifier')
-class CountryResource(Resource):
-    @ns.doc('get_country')
+    @ns.expect(country_model)
+    @ns.response(201, 'Country created')
+    @ns.response(400, 'Invalid request')
+    def post(self):
+        new_country_data = request.json
+        new_country_data['id'] = str(uuid.uuid4())
+        new_country_data['created_at'] = datetime.now()
+        new_country_data['updated_at'] = datetime.now()
+        country_id = data_manager.save_country(new_country_data)
+        return {
+            'message': 'Country created',
+            'country_id': country_id
+        }, 201
+
+
+@ns.route('/<string:country_id>')
+class CountryList(Resource):
     @ns.marshal_with(country_model)
-    def get(self, country_code):
-        """Fetch a country given its code"""
-        country = data_manager.get(country_code, 'country')
-        if not country:
-            ns.abort(404, f"Country {country_code} not found")
-        return country, 200
+    @ns.response(404, 'Country not found')
+    def get(self, country_id):
+        country_data = data_manager.get_country(country_id)
+        if country_data:
+            return country_data
+        else:
+            ns.abort(404, "Country not found")
+
+    @ns.response(204, 'Country deleted')
+    @ns.response(404, 'Country not found')
+    def delete(self, country_id):
+        if data_manager.delete_country(country_id):
+            return '', 204
+        else:
+            ns.abort(404, "Country not found")
+
+    @ns.expect(country_model)
+    @ns.response(204, 'Country updated')
+    @ns.response(400, 'Invalid request')
+    @ns.response(404, 'Country not found')
+    def put(self, country_id):
+        new_country_data = request.json
+        new_country_data['id'] = country_id
+        new_country_data['updated_at'] = datetime.now()
+        if data_manager.update_country(country_id, new_country_data):
+            return '', 204
+        else:
+            ns.abort(404, "Country not found")

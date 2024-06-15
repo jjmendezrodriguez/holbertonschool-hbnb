a9e665f38e4746f04677e8a7757fa929a290b7ef
Kryss <kryssbm99@gmail.com>
2024-06-14 23:37:40 -0400

    Final fixes

--- data_manager.py
+++ data_manager.py
@@ -24,107 +24,107 @@ class DataManager:
     # Métodos para Place
     def save_place(self, place_data):
         place = Place(**place_data)
-        self.place_repository.save(place)
+        self.place_repo.save(place)
         return place.place_id
 
     def get_place(self, place_id):
-        return self.place_repository.get(place_id)
+        return self.place_repo.get(place_id)
 
     def update_place(self, place_id, new_data):
-        return self.place_repository.update(place_id, new_data)
+        return self.place_repo.update(place_id, new_data)
 
     def delete_place(self, place_id):
-        return self.place_repository.delete(place_id)
+        return self.place_repo.delete(place_id)
 
     def get_all_places(self):
-        return self.place_repository.get_all()
+        return self.place_repo.get_all()
 
     # Métodos para User
     def save_user(self, user_data):
         user = User(**user_data)
-        self.user_repository.save(user)
+        self.user_repo.save(user)
         return user.user_id
 
     def get_user(self, user_id):
-        return self.user_repository.get(user_id)
+        return self.user_repo.get(user_id)
 
     def update_user(self, user_id, new_data):
-        return self.user_repository.update(user_id, new_data)
+        return self.user_repo.update(user_id, new_data)
 
     def delete_user(self, user_id):
-        return self.user_repository.delete(user_id)
+        return self.user_repo.delete(user_id)
 
     def get_all_users(self):
-        return self.user_repository.get_all()
+        return self.user_repo.get_all()
 
     # Métodos para Review
     def save_review(self, review_data):
         review = Review(**review_data)
-        self.review_repository.save(review)
+        self.review_repo.save(review)
         return review.review_id
 
     def get_review(self, review_id):
-        return self.review_repository.get(review_id)
+        return self.review_repo.get(review_id)
 
     def update_review(self, review_id, new_data):
-        return self.review_repository.update(review_id, new_data)
+        return self.review_repo.update(review_id, new_data)
 
     def delete_review(self, review_id):
-        return self.review_repository.delete(review_id)
+        return self.review_repo.delete(review_id)
 
     def get_all_reviews(self):
-        return self.review_repository.get_all()
+        return self.review_repo.get_all()
 
     # Métodos para Amenity
     def save_amenity(self, amenity_data):
         amenity = Amenity(**amenity_data)
-        self.amenity_repository.save(amenity)
+        self.amenity_repo.save(amenity)
         return amenity.amenity_id
 
     def get_amenity(self, amenity_id):
-        return self.amenity_repository.get(amenity_id)
+        return self.amenity_repo.get(amenity_id)
 
     def update_amenity(self, amenity_id, new_data):
-        return self.amenity_repository.update(amenity_id, new_data)
+        return self.amenity_repo.update(amenity_id, new_data)
 
     def delete_amenity(self, amenity_id):
-        return self.amenity_repository.delete(amenity_id)
+        return self.amenity_repo.delete(amenity_id)
 
     def get_all_amenities(self):
-        return self.amenity_repository.get_all()
+        return self.amenity_repo.get_all()
 
     # Métodos para Country
     def save_country(self, country_data):
         country = Country(**country_data)
-        self.country_repository.save(country)
+        self.country_repo.save(country)
         return country.country_id
 
     def get_country(self, country_id):
-        return self.country_repository.get(country_id)
+        return self.country_repo.get(country_id)
 
     def update_country(self, country_id, new_data):
-        return self.country_repository.update(country_id, new_data)
+        return self.country_repo.update(country_id, new_data)
 
     def delete_country(self, country_id):
-        return self.country_repository.delete(country_id)
+        return self.country_repo.delete(country_id)
 
     def get_all_countries(self):
-        return self.country_repository.get_all()
+        return self.country_repo.get_all()
 
     # Métodos para City
     def save_city(self, city_data):
         city = City(**city_data)
-        self.city_repository.save(city)
+        self.city_repo.save(city)
         return city.city_id
 
     def get_city(self, city_id):
-        return self.city_repository.get(city_id)
+        return self.city_repo.get(city_id)
 
     def update_city(self, city_id, new_data):
-        return self.city_repository.update(city_id, new_data)
+        return self.city_repo.update(city_id, new_data)
 
     def delete_city(self, city_id):
-        return self.city_repository.delete(city_id)
+        return self.city_repo.delete(city_id)
 
     def get_all_cities(self):
-        return self.city_repository.get_all()
+        return self.city_repo.get_all()
